42b1e0f88666a78400e0e5cc9a694ce6859a9a88
sphynx79 <miboscol@gmail.com>
2019-08-13 01:12:07 +0200

    Updated: Aggiornato lato client a mithril@2.0.3

--- client/src/init/routes.js
+++ client/src/init/routes.js
@@ -5,7 +5,7 @@ import RemitTransmission from "components/remit_transmission/remit_transmission.
 import SideBar from "components/sidebar/sidebar.js"
 import Burger from "components/burger/burger.js"
 
-m.route.prefix("")
+m.route.prefix = ""
 
 m.route(document.getElementById("app"), "/", {
     "/": {
