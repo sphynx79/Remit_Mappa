bddb627c622f3c864a3c2f2c056ea3398a525a5b
sphynx79 <miboscol@gmail.com>
2026-07-17 23:55:27 +0200

    Updated: Import modulare echarts 6 (core + moduli usati), vendor da 1.71 a 1.29 MiB [MED-011]

--- client/src/init/index.js
+++ client/src/init/index.js
@@ -1,18 +1,14 @@
 // src/init/index.js
 
+// echarts 6 modulare: si registrano solo i moduli usati dai 4 grafici
+// (bar/line su dataset, tooltip con axisPointer, legend, toolbox, dataZoom)
+import * as echarts from "echarts/core"
+import { BarChart, LineChart } from "echarts/charts"
+import { AxisPointerComponent, DataZoomComponent, DatasetComponent, GridComponent, LegendComponent, TitleComponent, ToolboxComponent, TooltipComponent } from "echarts/components"
+import { CanvasRenderer } from "echarts/renderers"
+
+echarts.use([BarChart, LineChart, AxisPointerComponent, DataZoomComponent, DatasetComponent, GridComponent, LegendComponent, TitleComponent, ToolboxComponent, TooltipComponent, CanvasRenderer])
+
 import "./index.scss"
 import "../model/app.js"
 import "./routes.js"
-
-// var echarts = require("echarts/lib/echarts")
-// import "echarts/lib/chart/bar"
-// import "echarts/lib/chart/line"
-// import "echarts/lib/component/tooltip"
-// import "echarts/lib/component/title"
-// import "echarts/lib/component/legendScroll"
-// import "echarts/lib/component/legend"
-// import "echarts/lib/component/dataZoom"
-// import "echarts/lib/component/dataZoomInside"
-// import "echarts/lib/component/dataZoomSelect"
-// import "echarts/lib/component/toolbox"
-// import "echarts/lib/component/dataset"
