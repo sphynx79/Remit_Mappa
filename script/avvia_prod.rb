e3e9981d0c7a39003dfedd562f335522ae8883c0
sphynx79 <miboscol@gmail.com>
2026-07-18 07:26:28 +0200

    Fixed: avvia_prod gestisce il Ctrl-C, aspetta lo shutdown di Puma e chiude Caddy liberando il terminale

--- script/avvia_prod.rb
+++ script/avvia_prod.rb
@@ -6,6 +6,17 @@
 # Uso: ruby script/avvia_prod.rb (usato dal task mise server-prod)
 
 Dir.chdir(File.expand_path('../server', __dir__))
-puts 'Avvio produzione: bundle exec puma -C config/puma_prod.rb'
+puts 'Avvio produzione: bundle exec puma -C config/puma_prod.rb (Ctrl-C per fermare)'
+
+# il Ctrl-C della console arriva anche a questo wrapper: lo si ignora e si lascia
+# a Puma lo shutdown graceful, aspettando che il figlio esca
+trap('INT') {}
 system('bundle', 'exec', 'puma', '-C', 'config/puma_prod.rb')
-exit($CHILD_STATUS ? $CHILD_STATUS.exitstatus : 1)
+stato = $?
+
+# Caddy gira in un process group separato (leader) e NON riceve il Ctrl-C:
+# senza questa pulizia resterebbe vivo attaccato alla console, bloccando il terminale
+system('taskkill', '/F', '/IM', 'caddy.exe', out: File::NULL, err: File::NULL)
+puts 'Stack di produzione fermato (Puma e Caddy chiusi).'
+
+exit(stato && stato.exitstatus ? stato.exitstatus : 0)
